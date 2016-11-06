2f1c66ecadb57ed55f9de5af23488fe26a230bff
Aliaga <Aliaga>
2016-11-06 13:35:55 +0100

    Unused cave generation code deleted

--- scripts/MapGenerator.cs
+++ scripts/MapGenerator.cs
@@ -100,22 +100,5 @@ public class MapGenerator : MonoBehaviour {
         }
         if (isBlank) return null;
         else return selected_height_map;
-    }
-    
-    int GetSurroundingWallCount(int gridX, int gridY) {
-        int wallCount = 0;
-        for (int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX++) {
-            for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY++) {
-                if (neighbourX >= 0 && neighbourX < width && neighbourY >= 0 && neighbourY < height) {
-                    if (neighbourX != gridX || neighbourY != gridY) {
-                        wallCount += (int)map[neighbourX, neighbourY];
-                    }
-                } else {
-                    wallCount++;
-                }
-            }
-        }
-
-        return wallCount;
     }    
 }
